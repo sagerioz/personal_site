cd52f1c9a88a5d759b8cccd13e87969658ad269c
sagerioz <ericaepperson27@yahoo.com>
2017-02-01 11:45:14 -0700

    my personal website for pre-course

---
+++ epperson.js
@@ -0,0 +1,11 @@
+
+$( "#clickme_spawn" ).click(function() {
+                              $( "#spawn" ).toggle( "slow", function() {
+                                // Animation complete.
+                              });
+                            });
+$( "#clickme_home" ).click(function() {
+                              $( "#bonedale" ).toggle( "slow", function() {
+                              // Animation complete.
+                              });
+                              });
